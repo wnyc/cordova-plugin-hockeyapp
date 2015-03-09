6e9343e9c3bb4244b78823ee143546320b7292bc
Brad Kammin <bkammin@nypublicradio.org>
2015-03-09 15:13:11 -0400

    if crash occurs while app is shutting down, do not send crash report

--- src/ios/HockeyAppPlugin.m
+++ src/ios/HockeyAppPlugin.m
@@ -9,6 +9,8 @@
 
 extern int ddLogLevel;
 
+static NSString *const kHockeyAppPluginAppReachedTerminateEventKey = @"AppReachedTerminateEvent";
+
 @interface HockeyAppPlugin ()
 
 @property (nonatomic) DDFileLogger *fileLogger;
@@ -29,6 +31,30 @@ extern int ddLogLevel;
         self.fileLogger.logFileManager.maximumNumberOfLogFiles = 1;
         [self.fileLogger rollLogFileWithCompletionBlock:nil];
         [DDLog addLogger:self.fileLogger];
+
+        // hack to prevent sending crash report for crashes that occur when app is shutting down
+        // reference for deleting crash report file:
+        // http://support.hockeyapp.net/discussions/problems/33370-is-there-a-way-to-manually-send-the-most-recent-crash-report-after-changing-the-crash-reporting-status
+        BOOL appReachedTerminateEvent = [[[NSUserDefaults standardUserDefaults] valueForKey:kHockeyAppPluginAppReachedTerminateEventKey] boolValue];
+        [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"AppReachedTerminateEvent"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+
+        if (appReachedTerminateEvent) {
+            NSFileManager *fileManager = [NSFileManager defaultManager];
+            NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+            NSString *cacheDir = [paths objectAtIndex: 0];
+            NSString *bundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
+            NSString *filePath = [cacheDir stringByAppendingPathComponent:@"com.plausiblelabs.crashreporter.data"];
+            filePath = [filePath stringByAppendingPathComponent:bundleIdentifier];
+            filePath = [filePath stringByAppendingPathComponent:@"live_report.plcrash"];
+            BOOL fileExists = [fileManager fileExistsAtPath:filePath];
+            NSError *error = nil;
+            if (fileExists) {
+                if (![fileManager removeItemAtPath:filePath error:&error]) {
+                    DDLogInfo(@"Could not delete crash report file:  %@ (%@)", error, filePath);
+                }
+            }
+        }
         
         [[BITHockeyManager sharedHockeyManager] configureWithBetaIdentifier:hockeyAppKey
                                                              liveIdentifier:hockeyAppKey
@@ -39,6 +65,13 @@ extern int ddLogLevel;
     }
     
     DDLogInfo(@"HockeyApp Plugin initialized");
+}
+
+#pragma mark Shutdown methods
+
+- (void)onAppTerminate {
+    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kHockeyAppPluginAppReachedTerminateEventKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 #pragma mark Plugin methods
